4d75b7f9b3e54cfc54529d11e573317ccf378c77
abhimanbhau <abhimanbhau@gmail.com>
2014-03-08 21:52:12 +0530

    Added Libraries and UI

--- PhotoManiaPro/Program.cs
+++ PhotoManiaPro/Program.cs
@@ -16,7 +16,7 @@ namespace PhotoManiaPro
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainWindowForm());
         }
     }
 }
